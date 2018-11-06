03b189bb4ee9928262a82ac8ec2284c75b313039
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-11-06 01:53:34 -0800

    style the deck details component

--- components/DeckDetails.js
+++ components/DeckDetails.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import Deck from './Deck'
+import { red, white } from '../utils/colors'
 
 class DeckDetails extends Component {
 
@@ -19,6 +20,15 @@ class DeckDetails extends Component {
     return (
       <View style={styles.container}>
         <Deck deck={this.props.deck} />
+        <TouchableOpacity style={styles.addCardBtn}>
+          <Text style={styles.addCardBtnText}>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.startQuizBtn}>
+          <Text style={styles.startQuizBtnText}>Start Quiz</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.deleteDeckBtn}>
+          <Text style={styles.deleteDeckBtnText}>Delete Deck</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -30,6 +40,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 15,
     backgroundColor: '#fff',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   titleText: {
@@ -39,6 +50,46 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 15,
     textAlign: 'center'
+  },
+  addCardBtn: {
+    backgroundColor: white,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+    borderWidth: 1,
+  },
+  addCardBtnText: {
+    color: red,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  startQuizBtn: {
+    backgroundColor: red,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  startQuizBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  deleteDeckBtn: {
+    backgroundColor: white,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  deleteDeckBtnText: {
+    color: red,
+    fontSize: 22,
+    textAlign: 'center',
   },
 });
 
