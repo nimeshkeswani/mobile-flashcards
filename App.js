bb0dfce900b6624f8af3adebf40fc0122831deea
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-11-05 19:30:55 -0800

    add icons and other configuration to tab navigation

--- App.js
+++ App.js
@@ -3,12 +3,47 @@ import { StyleSheet, Text, View, Platform } from 'react-native'
 import { createBottomTabNavigator } from 'react-navigation'
 import Decks from './components/Decks'
 import AddDeck from './components/AddDeck'
-import { purple, white } from './utils/colors'
+import { white, red } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
-const Tabs = createBottomTabNavigator({
-  Decks: Decks,
-  AddDeck: AddDeck
-})
+const RouteConfigs = {
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: "Decks",
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: "Add Deck",
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+};
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: red,
+    style: {
+      height: 56,
+      backgroundColor: white,
+      shadowColor: "rgba(0, 0, 0, 0.24)",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+};
+
+const Tabs = createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
 
 export default class App extends React.Component {
   render() {
