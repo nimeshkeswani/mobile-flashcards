f15166d3c759a721994253371c62266423e45661
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-11-06 20:17:53 -0800

    display one question from the deck if available and correct and incorrect buttons

--- components/Quiz.js
+++ components/Quiz.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import Deck from './Deck'
-import { red, white } from '../utils/colors'
+import { red, white, green } from '../utils/colors'
 import { deleteDeck } from '../actions/index'
 import { removeDeck } from '../utils/api'
 
@@ -17,13 +17,41 @@ class Quiz extends Component {
     }
   }
 
+  state = {
+    questionIndex: 0
+  }
+
   render() {
+
+    const { deck } = this.props
+
+    const { questionIndex } = this.state
 
     console.log(this.props)
 
+    if (deck.questions.length === 0) {
+      return (
+        <View style={styles.container}>
+        <Text>There are no cards in this deck.</Text>
+        </View>
+      )
+    }
+
     return (
       <View style={styles.container}>
-        <Text>{this.props.deck.totalCards}</Text>
+        <Text>{deck.totalCards}</Text>
+        <Text>{deck.questions[questionIndex].question}</Text>
+        <Text>{deck.questions[questionIndex].answer}</Text>
+        <TouchableOpacity
+          style={styles.correctBtn}
+          onPress={() => console.log('Correct')}>
+          <Text style={styles.btnText}>Correct</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.incorrectBtn}
+          onPress={() => console.log('Incorrect')}>
+          <Text style={styles.btnText}>Incorrect</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -35,8 +63,31 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 15,
     backgroundColor: '#fff',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  correctBtn: {
+    backgroundColor: green,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+    borderWidth: 1,
+  },
+  incorrectBtn: {
+    backgroundColor: red,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+    borderWidth: 1,
+  },
+  btnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
   },
 });
 
